28cdd7f8e46160650dc8badc7b43fb01d100feb0
John lockman <john.lockman@friday.local>
2020-12-17 10:16:46 -0600

    adding new roles dir
    Signed-off-by: John lockman <john.lockman@friday.local>

---
+++ roles/startmanager/files/flannel_net.sh
@@ -0,0 +1,3 @@
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.10.0/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
+
