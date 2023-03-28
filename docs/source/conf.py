b53fa03a3e1645886ac11a9e98020de9dbab51aa
cgoveas <cassandra.goveas@dell.com>
2023-03-28 10:32:40 +0530

    Updating documentation
    Signed-off-by: cgoveas <cassandra.goveas@dell.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,10 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
 
 project = 'Omnia'
 copyright = '2023, Dell Technologies'
