7cb7c65a81fa1d5fe77b0d22406c0462c1e9a04c
cgoveas <cassandra.goveas@dell.com>
2023-03-28 12:14:29 +0530

    Updating documentation
    Signed-off-by: cgoveas <cassandra.goveas@dell.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -100,14 +100,14 @@ html_theme = "sphinx_rtd_theme"
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# html_title = None
+html_title = "Omnia"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
+ html_short_title = "Omnia"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+ html_logo = "images/omnia-logo.png"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
