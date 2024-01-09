ed6fb056164616361ded298e262d69a640ef58d8
Boris Glimcher <Boris.Glimcher@emc.com>
2024-01-09 20:03:25 +0200

    feat(docker): add dockerfile and ci workflow for control plane
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- prereq.sh
+++ prereq.sh
@@ -34,7 +34,7 @@ echo "-------------------"
 python3.8 -m pip install ansible==5.10.0
 python3.8 -m pip install jinja2==3.1.2
 dnf install git-lfs -y
-git lfs pull
+git lfs pull || true
 
 selinux_count="$(grep "^SELINUX=disabled" /etc/selinux/config | wc -l)"
 if [[ $selinux_count == 0 ]];
