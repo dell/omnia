d349d6bf7f1a7157a28e79792e0a447b750b410a
cgoveas <cassandra.goveas@dell.com>
2024-02-14 12:43:25 +0530

    Updating formatting docs
    Signed-off-by: Goveas <cassandra.goveas@dell.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,11 @@ sys.path.insert(0, os.path.abspath("../"))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions += [
+   "sphinxawesome.highlighting",
+   # To help you with the upgrade to version 5:
+   # "sphinxawesome.deprecated",
+]
 
 html_css_files = ["theme.css"]
 html_js_files = [
