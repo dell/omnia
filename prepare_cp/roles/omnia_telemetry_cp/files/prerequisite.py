c25003dca75d7c73deaa976c3d22efeafcceb5a9
abhishek-sa1 <abhishek.sa3@dell.com>
2024-09-25 07:33:03 +0530

    copyright fixes

--- prepare_cp/roles/omnia_telemetry_cp/files/prerequisite.py
+++ prepare_cp/roles/omnia_telemetry_cp/files/prerequisite.py
@@ -1,4 +1,5 @@
 # Copyright 2024 Dell Inc. or its subsidiaries. All Rights Reserved.
+# Copyright 2024 Intel Corporation.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
