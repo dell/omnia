4576826a4c3597031c2eaab79d54d34be3def64c
SOWJANYAJAGADISH123 <Sowjanya.Jagadish@dell.com>
2026-05-19 21:27:37 +0530

    PXE utility fixes
    Signed-off-by: SOWJANYAJAGADISH123 <sowjanya.jagadish@dell.com>

--- build_stream/orchestrator/common/result_poller.py
+++ build_stream/orchestrator/common/result_poller.py
@@ -760,9 +760,6 @@ class ResultPoller:
             )
             self._artifact_metadata_repo.save(record)
 
-            if hasattr(self._artifact_metadata_repo, 'session'):
-                self._artifact_metadata_repo.session.commit()
-
             log_secure_info(
                 "info",
                 f"Restart node_results persisted as artifact for "
