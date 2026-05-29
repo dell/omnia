b75d66378357de6119b9cf9d91aea1b890e2ab05
Abhishek S A <abhishek.sa3@dell.com>
2026-05-29 21:25:06 +0530

    ansible 2.20 fixes
    Signed-off-by: Abhishek S A <abhishek.sa3@dell.com>

--- upgrade/roles/import_input_parameters/scripts/merge_powerscale_values.py
+++ upgrade/roles/import_input_parameters/scripts/merge_powerscale_values.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 # Copyright 2026 Dell Inc. or its subsidiaries. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
