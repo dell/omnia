818630fd3142b369721dd0f523386df8c4402581
Sujit Jadhav <sujit.jadhav@dell.com>
2026-08-24 17:39:07 +0530

    fix: address pylint errors in DCGM test
    - Remove unused pytest import
    - Add encoding='utf-8' to all open() calls
    - Fix line length violations by breaking long lines
    - Remove trailing whitespace
    
    Signed-off-by: Sujit Jadhav <sujit.jadhav@dell.com>

--- test/orchestrator/fvt/validate/test_dcgm_config.py
+++ test/orchestrator/fvt/validate/test_dcgm_config.py
@@ -18,40 +18,53 @@ Tests that dcgm_enabled is properly defined in schema and defaults to true
 
 import json
 import yaml
-import pytest
 
 
 def test_dcgm_enabled_in_schema():
     """Test that dcgm_enabled is defined in orchestrator_config.json schema"""
-    schema_path = "/share_sayuri/sujit/omnia/src/orchestrator/plugins/module_utils/orchestrator_validation/schema/orchestrator_config.json"
-    with open(schema_path) as f:
+    schema_path = (
+        "/share_sayuri/sujit/omnia/src/orchestrator/"
+        "plugins/module_utils/orchestrator_validation/"
+        "schema/orchestrator_config.json"
+    )
+    with open(schema_path, encoding="utf-8") as f:
         schema = json.load(f)
-    
-    assert "dcgm_enabled" in schema["properties"], "dcgm_enabled should be in schema properties"
-    assert schema["properties"]["dcgm_enabled"]["type"] == "boolean", "dcgm_enabled should be boolean type"
-    assert schema["properties"]["dcgm_enabled"]["default"] is True, "dcgm_enabled should default to true"
+
+    assert "dcgm_enabled" in schema["properties"], (
+        "dcgm_enabled should be in schema properties"
+    )
+    assert schema["properties"]["dcgm_enabled"]["type"] == "boolean", (
+        "dcgm_enabled should be boolean type"
+    )
+    assert schema["properties"]["dcgm_enabled"]["default"] is True, (
+        "dcgm_enabled should default to true"
+    )
 
 
 def test_dcgm_enabled_in_input_file():
     """Test that dcgm_enabled is defined in orchestrator_config.yml input file"""
     config_path = "/share_sayuri/sujit/omnia/src/orchestrator/input/orchestrator_config.yml"
-    with open(config_path) as f:
+    with open(config_path, encoding="utf-8") as f:
         config = yaml.safe_load(f)
-    
-    assert "dcgm_enabled" in config, "dcgm_enabled should be in orchestrator_config.yml"
-    assert config["dcgm_enabled"] is True, "dcgm_enabled should default to true in input file"
+
+    assert "dcgm_enabled" in config, (
+        "dcgm_enabled should be in orchestrator_config.yml"
+    )
+    assert config["dcgm_enabled"] is True, (
+        "dcgm_enabled should default to true in input file"
+    )
 
 
 def test_dcgm_enabled_boolean_values():
     """Test that dcgm_enabled accepts boolean values"""
     config_path = "/share_sayuri/sujit/omnia/src/orchestrator/input/orchestrator_config.yml"
-    with open(config_path) as f:
+    with open(config_path, encoding="utf-8") as f:
         config = yaml.safe_load(f)
-    
+
     # Test setting to false
     config["dcgm_enabled"] = False
     assert config["dcgm_enabled"] is False, "dcgm_enabled should accept false"
-    
+
     # Test setting to true
     config["dcgm_enabled"] = True
     assert config["dcgm_enabled"] is True, "dcgm_enabled should accept true"
