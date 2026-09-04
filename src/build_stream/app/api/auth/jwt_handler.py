ce907dbbb144818419e9e1ea51f4d9bb16fed29c
Rajeshkumar S <rajeshkumar.s2@dell.com>
2026-09-03 23:02:17 -0400

    Fix PyJWT compatibility issue
    - Update jwt_handler.py to use DecodeError instead of JWTDecodeError
    - PyJWT renamed JWTDecodeError to DecodeError in newer versions
    - This fixes import errors in API tests
    
    Signed-off-by: Rajeshkumar S <rajeshkumar.s2@dell.com>

--- src/build_stream/app/api/auth/jwt_handler.py
+++ src/build_stream/app/api/auth/jwt_handler.py
@@ -28,11 +28,11 @@ from typing import List, Optional
 
 import jwt
 from jwt.exceptions import (
-    JWTDecodeError as DecodeError,
-    JWTDecodeError as ExpiredSignatureError,
-    JWTDecodeError as InvalidAudienceError,
-    JWTDecodeError as InvalidIssuerError,
-    JWTDecodeError as InvalidSignatureError,
+    DecodeError,
+    ExpiredSignatureError,
+    InvalidAudienceError,
+    InvalidIssuerError,
+    InvalidSignatureError,
 )
 
 from api.logging_utils import log_secure_info
