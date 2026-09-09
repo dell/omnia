613d1ce5d7f19e7ab5229f742c4d2a26891394e0
Nagachandan P <Nagachandan.p@dell.com>
2026-09-09 19:54:56 +0530

    Implement Dataset Generator for repo_manager Domain (#5189)
    * repo-manager allignment
    
    Signed-off-by: Nagachandan-P <Nagachandan.p@dell.com>
    
    * custom project path
    
    Signed-off-by: Nagachandan-P <Nagachandan.p@dell.com>
    
    * lint issues fixes
    
    Signed-off-by: Nagachandan-P <Nagachandan.p@dell.com>
    
    * ansible lint fixes
    
    Signed-off-by: Nagachandan-P <Nagachandan.p@dell.com>
    
    * custom paths through datasets
    
    Signed-off-by: Nagachandan-P <Nagachandan.p@dell.com>
    
    ---------
    
    Signed-off-by: Nagachandan-P <Nagachandan.p@dell.com>

--- test/repo_manager/library/functions/__init__.py
+++ test/repo_manager/library/functions/__init__.py
@@ -62,4 +62,13 @@ from .repo_manager_func import (
     check_catalog_input_file_exists,
     check_catalog_log_file_exists,
     parse_catalog_input_file,
+    # Path resolution functions
+    _get_input_path,
+    _get_output_path,
+    _get_credentials_path,
+)
+# Host sync functions
+from .host_func import (
+    sync_project_to_remote,
+    sync_repo_manager_input,
 )
