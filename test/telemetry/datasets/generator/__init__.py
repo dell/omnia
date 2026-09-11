bc63f9f0fda3a512d586ed26dfe256b07732d9de
Mithilesh Reddy <mithilesh.reddy@dell.com>
2026-09-11 16:45:27 +0530

    Add telemetry dataset generator logic (#5197)
    * rewrite telemetry dataset generator with source-first architecture
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Env variable priority fix in test automation
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Update conftest.py
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Fix conftest.py logic
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Modify telemetry use cases for test generation
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Fix dataset generation use case flow
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Update Readme files
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Add more use cases to telemetry dataset generation
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Modify naming in telemetry datasets
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    * Change log path for telemetry input validation
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>
    
    ---------
    
    Signed-off-by: mithileshreddy04 <mithilesh.reddy@dell.com>

--- test/telemetry/datasets/generator/__init__.py
+++ test/telemetry/datasets/generator/__init__.py
@@ -11,6 +11,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
----
-description: "Canonical source defaults (iDRAC, LDMS, PowerScale, and OME enabled)"
-patches: {}
+"""Telemetry dataset generation package."""
